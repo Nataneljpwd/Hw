24672c6fe86323e77bbddddf298f7296ccf8c19c
Natanel Rudyuk Lakir <natanel@localhost.localdomain>
2024-05-07 14:36:17 +0300

    all solutions using one-liners (except the one where input is needed

--- Part2/pythagorean_triplets.py
+++ Part2/pythagorean_triplets.py
@@ -10,12 +10,17 @@ def pythagorean_triplets(sum:int) -> None:
             if is_pythagorean_triplet(i, j, sum):
                 print(f"{i} < {j} < {sum - i - j}")
 
+def pythagorean_triplets_one_line(sum:int) -> None:
+    [print(f"{i} < {j} < {sum - i - j}") for i in range(1, (sum + 1)//3) for j in range(i+ 1,(sum + 1)//2) if sum * sum - 2*sum*(i+j) + 2*i*j == 0]
+
 def is_pythagorean_triplet(a:int, b:int, sum:int):
     return sum * sum - 2*sum*(a+b) + 2*a*b == 0
 
 def main():
     pythagorean_triplets(1000)
+    pythagorean_triplets_one_line(1000)
     pythagorean_triplets(12)
+    pythagorean_triplets_one_line(12)
 
 if __name__ == "__main__":
     main()
